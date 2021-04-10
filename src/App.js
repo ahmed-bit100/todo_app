c6f693962404f3ebed0bf36d58bb02fbba7d3edd
عبد المولى أحمد <ahmedabdelmoula1@gmail.com>
2021-04-10 11:13:19 +0100

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import AddTask from "./Components/AddTask";
+import TaskList from "./Components/TaskList";
+
+export default class App extends Component {
+  state = {
+    tasks: [
+      { id: 0, action: "wake up", isDone: true },
+      { id: 1, action: "have coffee", isDone: false },
+    ],
+  };
+  // deleting tasks from the list
+  handleDelete = (index) =>
+    this.setState({ tasks: this.state.tasks.filter((el) => el.id !== index) });
+  // handling your finished/unfinished tasks
+  handleComplete = (index) =>
+    this.setState({
+      tasks: this.state.tasks.map((el) =>
+        el.id === index ? { ...el, isDone: !el.isDone } : el
+      ),
+    });
+
+  // adding tasks to the list
+  handleAdd = (text) => {
+    const newTask = {
+      id: this.state.tasks.length,
+      action: text,
+      isDone: false,
+    };
+    text.trim()
+      ? this.setState({ tasks: [...this.state.tasks, newTask] })
+      : alert("please enter a task");
+    this.setState({ newText: "" });
+  };
+  render() {
+    return (
+      <div className="title">
+        <AddTask handleAdd={this.handleAdd} />
+        <TaskList
+          tasks={this.state.tasks}
+          handleComplete={this.handleComplete}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
